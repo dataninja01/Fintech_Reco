d14f8a04cff4a520bbee32034a3eb1a8c8870ffd
dataninja01 <61597857+dataninja01@users.noreply.github.com>
2021-12-18 17:37:50 -0800

    Add request.py file

---
+++ Mini_Capstone_project/request.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+# read json file w api requests
+with open("api_test_data.json") as f:
+    my_requests = json.load(f)
+
+# Flask app api
+url = "http://localhost:8080/api"
+r = requests.post(url, json= my_requests)
+
+# print response
+for resp in r.json(): print(resp)
