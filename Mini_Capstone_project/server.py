ca347fa9cd0bbe67b422307881ef3772816675b0
Sperks <violasarah2000@gmail.com>
2021-12-21 20:16:30 -0600

    now it works!

--- Mini_Capstone_project/server.py
+++ Mini_Capstone_project/server.py
@@ -3,7 +3,7 @@ import joblib
 from flask import Flask, request, render_template, jsonify
 
 # instantiate Flask app
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 # load model w metadata
 model = joblib.load("models/pipe_clf_checkpoint.joblib")
@@ -13,25 +13,15 @@ model_clf = model['pipeline']
 @app.route('/')
 def my_form():
     return render_template('form.html')
-
+    
 @app.route("/", methods = ["POST"])
-def predict_sentiment():
-    # pass requests to model
-    data = request.get_json(force=True)
-    preds = model_clf.predict(data['input'])
-    
-    # predict class values vs labels
-    if data['predict classes'] == "True":
-        predictions = [model['class labels'][key] for key in preds]
-    else:
-        predictions = preds
-    
-    # store responses
-    response = []
-    for x, pred in zip(data['input'], predictions):
-        response.append("Text: "+str(x)+" | class: "+str(pred))
-    
-    return render_template(response)
+def predict():
+    text = request.form['text1']
+    input=[text]
+    preds = model_clf.predict(input)
+    predictions = [model['class labels'][key] for key in preds]
+    output = ''.join(predictions).capitalize()
+    return render_template('form.html', text=text, final = output)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
